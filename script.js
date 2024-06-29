c992943d5456486b3ea446f5c35fb61b26081722
jostheta <f20201632@goa.bits-pilani.ac.in>
2024-06-29 17:37:06 +0530

    added bold italics and underline

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-   
+$(document).ready(function() {
+    $('#add-question').click(function() {
+        // Clone the template
+        var newQuestion = $('#question-template').clone();
+
+        // Optionally, you can remove the id from the clone
+        newQuestion.removeAttr('id');
+
+        // Append the cloned question to the form container
+        $('#question-template').parent().append(newQuestion);
+    });
 });
