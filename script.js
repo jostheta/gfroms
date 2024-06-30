04802a31ed59f222c9e16a5fef974f96918ce215
jostheta <f20201632@goa.bits-pilani.ac.in>
2024-07-01 04:38:27 +0530

    added the options and delete options buttons

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ $(document).ready(function() {
         newQuestion.removeAttr('id');
 
         // Append the cloned question to the form container
-        $('#question-template').parent().append(newQuestion);
+        $('#question-template').parent().append(newQuestion).append('<br>');
     });
+
+    $('#add-option').click(function() {
+        //Clone the template
+        var newOption = $('#option-template').clone();
+
+        //removing id
+        newOption.removeAttr('id');
+
+        //remove input field
+        newOption.find('input').val(''); 
+
+        //Append the cloned option to new options space
+        $('#new-options').append(newOption).append('<br>');
+
+    });
+
+    $('#options-container').on('click', '.question-box_option-block_option-close', function() {
+        $(this).closest('.question-box_option-block').next('br').remove(); // Remove the <br> after the option
+        $(this).closest('.question-box_option-block').remove(); // Remove the option
+    });
+
+    
 });
